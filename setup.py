915572fa6f55be2da6cb06afee9d146a9bfa5f84
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-11-13 22:26:07 +0530

    Setup file changes

--- setup.py
+++ setup.py
@@ -1,27 +1,35 @@
-from distutils.core import setup
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+print(HERE)
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
 setup(
-  name = 'pipmate',
-  packages = ['src/pipmate'],
-  version = '0.1.0',
-  license='MIT',
-  description = 'Move between pipfiles, pyproject.toml and requirements.txt with ease!',
-  author = 'Srujan Deshpande',
-  author_email = 'srujan.deshpande@gmail.com',
-  url = 'https://github.com/srujandeshpande/pipmate',
-  download_url = 'https://github.com/srujandeshpande/pipmate/releases/tag/v0.1.0',
-  keywords = ['Poetry', 'PyPip', 'Dependencies'],
-  install_requires=[
-          'cleo',
-          'toml',
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-  ],
+    name="pipmate",
+    version='0.1.1-alpha.1',
+    license='MIT',
+    description = 'Move between pipfiles, pyproject.toml and requirements.txt with ease!',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url = 'https://github.com/srujandeshpande/pipmate',
+    download_url = 'https://github.com/srujandeshpande/pipmate/releases/tag/v0.1.0',
+    author = 'Srujan Deshpande',
+    author_email = 'srujan.deshpande@gmail.com',
+    keywords = ['Poetry', 'PyPip', 'Dependencies'],
+    install_requires=[
+            'cleo',
+            'toml',
+        ]
+    #packages=["reader"],
+    #include_package_data=True,
+    #install_requires=["feedparser", "html2text"],
+    #entry_points={
+    #    "console_scripts": [
+    #        "realpython=reader.__main__:main",
+    #    ]
+    #},
 )
